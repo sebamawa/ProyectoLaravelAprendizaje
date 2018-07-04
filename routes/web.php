2601613c1b11a2124fe1d72cf7a236d27b46db42
sebamawa <sebamawa@hotmail.com>
2018-07-04 01:54:26 -0300

    Routing - Pruebas

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,28 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+//rutas de pruebas
+
+//closure
+Route::get('/prueba1', function() {
+    return 'Esto es una prueba';
+});
+
+//parametros en ruta y closure
+Route::get('users/{id}/{phone}', function($id, $phoneReal) {
+    return 'Usuario: ' . $id . ' - Phone: ' . $phoneReal; //solo tener en cuenta orden de
+        // parametros en rutas y en lista de closure (no nombres)
+});
+
+//ruta a metodo de controller con parametro opcional
+Route::get('/prueba2/{id?}', 'WelcomeController@index');
+
+//closure con parametro regex
+Route::get('/prueba3/{id}', function($id) {
+    return 'Id = ' . $id;
+})->where('id', '[0-9]+'); //where(['id'=>'[0-9]+', 'username'=>'[A-Za-z]+']);
+
+//nombre (alias) para ruta
+Route::get('/prueba4{id?}', 'WelcomeController@show')->name('pruebita');
